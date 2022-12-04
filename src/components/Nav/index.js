3ce084ddf007ecc061c54c467ee6b32e44992c60
Benny <benny.le890@gmail.com>
2022-12-04 14:53:02 -0500

    added resume button

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,6 +18,7 @@ function Nav() {
         <header>
             <div id='navbar' className='nav navbar-items'>
                 <div className='logo'></div>
+                <a href='button' className='reveal'>Resume</a>
                 <a href='#contact' className='reveal'>Contact</a>
                 <a href='#professional-work' className='reveal'>Professional Work</a>
                 <a href='#about-me' className='reveal'>About</a>
