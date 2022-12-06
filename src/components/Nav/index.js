971b5e3a7957d170d0a1e78b42f52aede55d3b20
Benny <benny.le890@gmail.com>
2022-12-06 16:33:51 -0500

    added css to resume button

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,7 +18,9 @@ function Nav() {
         <header>
             <div id='navbar' className='nav navbar-items'>
                 <div className='logo'></div>
-                <a href='BLe_Resume_C.pdf' download='Ble_Resume_C.pdf' className='reveal'>Resume</a>
+                <button className='resume-button'>
+                    <a href='BLe_Resume_C.pdf' download='Ble_Resume_C.pdf' className='reveal'>Resume</a>
+                </button>
                 <a href='#contact' className='reveal'>Contact</a>
                 <a href='#professional-work' className='reveal'>Professional Work</a>
                 <a href='#about-me' className='reveal'>About</a>
